f2df5002c5cad0d39009eb62414ab83a35873157
HaidarFaruqi <haidarfaruqi126@gmail.com>
2025-09-18 02:15:13 +0700

    feat: Add Jadwal Mingguan

--- BE/routes/menuItemRoutes.js
+++ BE/routes/menuItemRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getMenuItems, createMenuItem } = require('../controllers/menuItemController');
+const { getMenuItems, createMenuItem, getDailyMenu } = require('../controllers/menuItemController');
 const authMiddleware = require('../middleware/auth');
 
 router.get('/', getMenuItems);
 router.post('/', authMiddleware(['penjual']), createMenuItem);
+router.get('/daily', getDailyMenu);  
 
 module.exports = router;
