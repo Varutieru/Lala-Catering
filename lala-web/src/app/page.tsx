eaf88dee5f51fe2fa5b251bd78154c4a63f90fb5
Varutieru <151426935+Varutieru@users.noreply.github.com>
2025-10-22 17:51:07 +0700

    Hero Fix, Button Functions, Initialize New Pages

--- lala-web/src/app/page.tsx
+++ lala-web/src/app/page.tsx
@@ -12,9 +12,17 @@ export default function HomePage () {
 
   const router = useRouter();
   const pathname = usePathname();
+
+  const scrollToSection = () => {
+  const targetElement = document.getElementById('profilePage');
+  targetElement?.scrollIntoView({ 
+    behavior: 'smooth',
+    block: 'start' 
+  });
+};
   
   return(
-    <main className="overflow-x-hidden max-w-screen h-screen">
+    <main className="overflow-x-hidden w-screen h-screen">
       <div className="bg-[#EF6C6C] relative w-full min-h-screen max-w-full">
 
         {/*HERO BG */}
@@ -31,12 +39,12 @@ export default function HomePage () {
         <div className="relative w-full h-full max-w-full px-[5vw]">
 
           {/* HEADER */}
-          <header className={`fixed top-0 left-0 right-0 z-50 transition-colors duration-300 ${isScrolled ? 'bg-[#0E395D]' : 'bg-transparent'}`}>
+          <header className={`absolute top-0 left-0 right-0 z-50 transition-colors duration-300 ${isScrolled ? 'bg-[#0E395D]' : 'bg-transparent'}`}>
             <Header />
           </header>
 
           {/* HERO */}
-            <div className="absolute -translate-x-1/2 -translate-y-1/2 
+            <div className="absolute top-[50vh] left-1/2 -translate-x-1/2 -translate-y-1/2 
                           w-[80vw] md:w-[60vw] lg:w-[42vw] 
                           flex flex-col justify-center items-center gap-8 md:gap-12">
 
@@ -48,10 +56,10 @@ export default function HomePage () {
               </h1>
 
               {/* BUTTONS CONTAINER */}
-              <div className="w-full flex justify-around items-center gap-4">
+              <div className="w-full h-full flex justify-center items-center gap-[-10]">
 
                 {/* CATERING */}
-                <div className="relative w-[200px] h-[80px] md:w-[250px] md:h-[100px] lg:w-[300px] lg:h-[120px]">
+                <div className="relative w-[350px] h-[112px] md:w-[450px] md:h-[144px] lg:w-[600px] lg:h-[192px]">
                   <Image
                     src={"assets/hero/catering.svg"}
                             alt="Catering Button"
@@ -62,7 +70,7 @@ export default function HomePage () {
                 </div>
 
                 {/* ONLINE ORDER */ }
-                <div className="relative w-[200px] h-[80px] md:w-[250px] md:h-[100px] lg:w-[300px] lg:h-[120px]">
+                <div className="relative w-[350px] h-[112px] md:w-[450px] md:h-[144px] lg:w-[600px] lg:h-[192px]">
                   <Image
                     src={"assets/hero/onlineorder.svg"}
                             alt="Catering Button"
@@ -77,7 +85,44 @@ export default function HomePage () {
             </div>
 
         </div>
+
+        {/* EXPLORE MORE BUTTON */}
+        <div className="absolute left-1/2 -translate-x-1/2 bottom-[10vh] z-[100]">
+          <button
+            onClick={() => scrollToSection()}
+            type="button"
+            className="relative flex items-center justify-center 
+                     px-8 py-3 
+                     rounded-full border-2 border-[#4CB8A2] 
+                     text-[#FFF1E8] font-sao-torpes
+                     overflow-hidden
+                     group">
+            <span className="absolute inset-0 w-0 bg-[#4CB8A2] rounded-full
+                     transition-all duration-300 ease-out
+                     group-hover:w-full"></span>
+            <span className="relative z-10">Explore Now</span>
+          </button>
+        </div>
+
       </div>
+
+      {/* PROFILE SECTION */}
+      <div
+        id="profilePage"
+        className="w-screen h-screen overflow-x-hidden overflow-y-hidden">
+        
+        {/* CATERING */}
+        <div className="w-full h-[50vh] bg-[#EF6C6C] flex items-center justify-center">
+
+        </div>
+
+        {/* ONLINE ORDER */}
+        <div className="w-full h-[50vh] bg-[#F9A94E] flex justify-between items-center">
+
+        </div>
+
+      </div>
+
     </main>
   )
 }
