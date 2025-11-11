af16cad60d57f6c3f9818002ca1ac856a6cdb7f3
FaizArsyiP <faizpragata@gmail.com>
2025-11-11 16:24:06 +0700

    feat: update server port to 5000 and add package.json and package-lock.json

--- BE/index.js
+++ BE/index.js
@@ -28,5 +28,5 @@ app.use("/api/menu", menuRoutes);
 app.use("/api/jadwal", jadwalRoutes);
 //app.use("/api/faq", faqRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
